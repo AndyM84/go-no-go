98a87eeef7f6679a674f6fa7f7ba99f75fb8ecc8
Andrew Male <amale@zibings.com>
2015-04-06 07:37:09 -0400

    Adding UE4 project (4.7.4)

---
+++ GoNoGo/Intermediate/Build/Win64/GoNoGoEditor/Development/GoNoGo/GoNoGo.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_GONOGO_H__
+#define __AUTO_GONOGO_H__
+//Last Write: 4/6/2015 11:35:19 AM +00:00
+#include "C:\Users\Andrew\Projects\go-no-go\go-no-go\GoNoGo\Source\GoNoGo\GoNoGo.h"
+#endif//__AUTO_GONOGO_H__
---
+++ GoNoGo/Source/GoNoGo/BlueprintHelpers.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BlueprintHelpers.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GONOGO_API UBlueprintHelpers : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
