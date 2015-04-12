f8d5d54561944a51682ddc482c6a3ea0c6c04743
ssgsgugel <steven.gugel@ssgsoftwareconsulting.com>
2015-04-12 13:24:55 -0400

    Additional methods for ConditionalBase to mimick HoldBase

--- GoNoGo/Source/GoNoGo/include/BaseClasses/ConditionalBase.hpp
+++ GoNoGo/Source/GoNoGo/include/BaseClasses/ConditionalBase.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
+#include <Core.hpp>
 #include <BaseClasses/NodeBase.hpp>
 
 class ConditionalBase : public N2f::NodeBase
 {
+private:
+	time_t _statusUpdated;
+	RESULT_STATUSES _status;
+
+protected:
+	void UpdateStatus();
+
 public:
 
-        virtual	~ConditionalBase(){}
+	ConditionalBase();
+	ConditionalBase(std::string Key, RESULT_STATUSES Status);
+
+	time_t GetStatusUpdateTime();
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	/// <summary>	Virtual to check isAcceptable. </summary>
--- GoNoGo/Source/GoNoGo/src/BaseClasses/ConditionalBase.cpp
+++ GoNoGo/Source/GoNoGo/src/BaseClasses/ConditionalBase.cpp
@@ -1,3 +1,26 @@
 #include "GoNoGo.h"
 #include <BaseClasses/ConditionalBase.hpp>
 
+ConditionalBase::ConditionalBase() : ConditionalBase("ConditionalBase", INVALID)
+{
+
+}
+
+ConditionalBase::ConditionalBase(std::string Key, RESULT_STATUSES Status)
+{
+	this->SetKey(Key.c_str());
+	this->SetVersion("1.0");
+	this->_status = Status;
+	time(&this->_statusUpdated);
+}
+
+void ConditionalBase::UpdateStatus() {
+	time(&this->_statusUpdated);
+
+}
+
+time_t ConditionalBase::GetStatusUpdateTime() {
+
+	return this->_statusUpdated;
+
+}
