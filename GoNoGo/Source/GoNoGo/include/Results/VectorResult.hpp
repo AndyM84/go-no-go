bd849fa862a70b888ee5c150158e25f18be27f10
Andrew Male <amale@zibings.com>
2015-04-12 04:03:44 -0400

    Fixing some formatting and changing where methods are defined for VectorResult (declare in header, define in cpp)

--- GoNoGo/Source/GoNoGo/include/Results/VectorResult.hpp
+++ GoNoGo/Source/GoNoGo/include/Results/VectorResult.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <math.h>
 #include <sstream>
 #include <BaseClasses/ResultBase.hpp>
@@ -6,9 +7,7 @@
 class VectorResult : public ResultBase
 {
 private:
-	
-	double magnitude;
-	double direction;
+	double _magnitude, _direction;
 
 	// In case we find we need them:
 	//double degToRad(double deg) { return deg * PI / 180.0; };
@@ -17,26 +16,14 @@ private:
 	//Normalizes any number to an arbitrary range 
 	//by assuming the range wraps around when going below min or above max
 	// Thank you: http://stackoverflow.com/questions/1628386/normalise-orientation-between-0-and-360
-	double normalise(const double value, const double start, const double end)
-	{
-		const double width = end - start;   // 
-		const double offsetValue = value - start;   // value relative to 0
-
-		return (offsetValue - (floor(offsetValue / width) * width)) + start;
-		// + start to reset back to start of original range
-	}
+	double Normalise(const double value, const double start, const double end);
 
 public:
-	void setMagnitude(const double mag) { magnitude = mag; };
-	void setDirection(const double dir) { direction = normalise(dir, 0, 2 * PI); };
-	void setMagDir(const double mag, const double dir) { magnitude = mag; direction = normalise(dir, 0, 2 * PI); };
-	double getMagnitude() { return magnitude; };
-	double getDirection() { return direction; };
+	void SetMagnitude(const double mag);
+	void SetDirection(const double dir);
+	void SetMagDir(const double mag, const double dir);
+	double GetMagnitude();
+	double GetDirection();
 
-	std::string ToJson() {
-		std::stringstream ss;
-		ss << "{magnitude:'" << magnitude << "',direction:'" << direction << "'}";
-		return ss.str();
-	}
-
+	std::string ToJson();
 };
--- GoNoGo/Source/GoNoGo/src/Results/VectorResult.cpp
+++ GoNoGo/Source/GoNoGo/src/Results/VectorResult.cpp
@@ -1,3 +1,51 @@
 #include "GoNoGo.h"
 #include <Results/VectorResult.hpp>
 
+double VectorResult::Normalise(const double value, const double start, const double end)
+{
+	const double width = end - start;   // 
+	const double offsetValue = value - start;   // value relative to 0
+
+	return (offsetValue - (floor(offsetValue / width) * width)) + start;
+	// + start to reset back to start of original range
+}
+
+void VectorResult::SetMagnitude(const double mag)
+{
+	_magnitude = mag;
+
+	return;
+}
+
+void VectorResult::SetDirection(const double dir)
+{
+	_direction = Normalise(dir, 0, 2 * PI);
+
+	return;
+}
+
+void VectorResult::SetMagDir(const double mag, const double dir)
+{
+	_magnitude = mag;
+	_direction = Normalise(dir, 0, 2 * PI);
+
+	return;
+}
+
+double VectorResult::GetMagnitude()
+{
+	return _magnitude;
+};
+
+double VectorResult::GetDirection()
+{
+	return _direction;
+};
+
+std::string VectorResult::ToJson()
+{
+	std::stringstream ss;
+	ss << "{magnitude:'" << _magnitude << "',direction:'" << _direction << "'}";
+
+	return ss.str();
+}
