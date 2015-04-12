6524001facc16218f45fc7eae7ce5babb156d609
ssgsgugel <steven.gugel@ssgsoftwareconsulting.com>
2015-04-12 02:16:44 -0400

    Added virtual isAcceptable

--- GoNoGo/Source/GoNoGo/include/BaseClasses/ConditionalBase.hpp
+++ GoNoGo/Source/GoNoGo/include/BaseClasses/ConditionalBase.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <BaseClasses/NodeBase.hpp>
+#include <BaseClasses/VectorResult.hpp>
 
 class ConditionalBase : public N2f::NodeBase
 {
-
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// <summary>	Virtual to check isAcceptable. </summary>
+	///
+	/// <remarks>   Steven, 4/12/2015. </remarks>
+	///
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	public virtual VectorResult isAcceptable() = 0;
 };
