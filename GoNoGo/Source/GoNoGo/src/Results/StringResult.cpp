ddadec49b554acc6eb479c1a09ced2bea2338ae8
Andrew Male <amale@zibings.com>
2015-04-12 10:54:34 -0400

    Adding StringResult method

--- GoNoGo/Source/GoNoGo/src/Results/StringResult.cpp
+++ GoNoGo/Source/GoNoGo/src/Results/StringResult.cpp
@@ -1,3 +1,26 @@
 #include "GoNoGo.h"
 #include <Results/StringResult.hpp>
 
+StringResult::StringResult() : StringResult("", INVALID) { }
+
+StringResult::StringResult(std::string Result) : StringResult(Result, INVALID) { }
+
+StringResult::StringResult(std::string Result, RESULT_STATUSES Status) : ResultBase(Status)
+{
+	this->_result = Result;
+
+	return;
+}
+
+const std::string StringResult::GetResult()
+{
+	return this->_result;
+}
+
+std::string StringResult::ToJson()
+{
+	std::stringstream ss;
+	ss << "{ status: '" << this->_status << "', result: '" << this->_result << "' }";
+
+	return ss.str();
+}
