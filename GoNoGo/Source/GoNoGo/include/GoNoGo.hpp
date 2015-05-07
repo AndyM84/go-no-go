b9d9ebb63a9d1075453ab98d3247cad78af1a9b4
Andrew Male <amale@zibings.com>
2015-05-07 14:32:30 -0400

    Moving GoNoGo.hpp and adding TelemetryBase

--- GoNoGo/Source/GoNoGo/include/GoNoGo.hpp
+++ GoNoGo/Source/GoNoGo/include/GoNoGo.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <N2f.hpp>
+#include <Core.hpp>
 #include <BaseClasses/ConditionalBase.hpp>
 #include <BaseClasses/HoldBase.hpp>
 #include <BaseClasses/ResultBase.hpp>
+#include <BaseClasses/TelemetryBase.hpp>
 #include <Dispatches/LaunchDispatch.hpp>
 #include <Results/IntResult.hpp>
 #include <Results/RangeResult.hpp>
