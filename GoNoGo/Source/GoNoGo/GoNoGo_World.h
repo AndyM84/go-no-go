18333cad77b5e54ddda34cec49e5382ef6e2aa69
Andrew Male <amale@zibings.com>
2015-05-08 11:33:30 -0400

    Adding a world object, going to see if we can use this for passing data back and forth to the UI

---
+++ GoNoGo/Source/GoNoGo/GoNoGo_World.h
@@ -0,0 +1,19 @@
+// (c) 2015 Team GoNoGo
+
+#pragma once
+
+#include "GameFramework/WorldSettings.h"
+#include "GoNoGo_World.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GONOGO_API AGoNoGo_World : public AWorldSettings
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
