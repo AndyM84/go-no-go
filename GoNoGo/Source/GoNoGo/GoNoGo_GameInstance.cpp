08fdc856e292f771e3de6e867446c36b91e67220
Andrew Male <amale@zibings.com>
2015-10-01 13:09:24 -0400

    Saving progress before reboot, also pushing this to a new branch momentarily

---
+++ GoNoGo/Source/GoNoGo/GoNoGo_GameInstance.cpp
@@ -0,0 +1,8 @@
+// (c) 2015 Team GoNoGo
+
+#include "GoNoGo.h"
+#include "GoNoGo_GameInstance.h"
+
+
+
+
---
+++ GoNoGo/Source/GoNoGo/GoNoGo_GameInstance.h
@@ -0,0 +1,19 @@
+// (c) 2015 Team GoNoGo
+
+#pragma once
+
+#include "Engine/GameInstance.h"
+#include "GoNoGo_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GONOGO_API UGoNoGo_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
